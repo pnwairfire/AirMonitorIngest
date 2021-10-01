2cd5156e7d21e78542ed34c7cb0e76bcf2ce4a4a
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2021-10-01 10:01:09 -0700

    test: able to create a PWFSLSmoke `ws_monitor` object

--- local_jon/first_tests.R
+++ local_jon/first_tests.R
@@ -10,32 +10,92 @@ library(AirMonitorIngest)
 MazamaLocationUtils::mazama_initialize("~/Data/Spatial")
 MazamaLocationUtils::setLocationDataDir("~/Data/known_locations")
 
-# Get appropriate data
+# ----- Get meta and data ------------------------------------------------------
 
 parameterCode <- "88101"
 sites_locationTbl <- MazamaLocationUtils::table_load("AQS_88101_sites")
 AQS_monitors <- epa_aqs_getMonitors(downloadDir = "~/Data/EPA")
 AQS_data <- epa_aqs_parseHourlyData("~/Data/EPA/hourly_88101_2008.zip")
 
-# Create meta
+meta_ORIG <- epa_aqs_createMeta(sites_locationTbl, AQS_monitors, parameterCode)
 
-meta <- epa_aqs_createMeta(sites_locationTbl, AQS_monitors, parameterCode)
+data_ORIG <- epa_aqs_createData(AQS_data, meta_ORIG, parameterCode)
 
-data <- epa_aqs_createData(AQS_data, meta, parameterCode)
+# ----- Minimal 'air_monitor' object -------------------------------------------
 
-# Modify meta to match 'ws_monitor' sensibilities
+# Modify meta to match prototype 'air_monitor' sensibilities
 
 deviceDeploymentIDs <- names(data)[2:ncol(data)]
 
 meta <-
-  meta %>%
+  meta_ORIG %>%
   dplyr::filter(deviceDeploymentID %in% deviceDeploymentIDs)
 
 meta$monitorID <- meta$deviceDeploymentID
 
 # Create the 'ws_monitor' data list
-ws_monitor <- list(meta = meta, data = data)
+ws_monitor <- list(meta = meta, data = data_ORIG)
 ws_monitor <- structure(ws_monitor, class = c("ws_monitor", "list"))
 
 AirMonitorPlots::monitor_ggTimeseries(ws_monitor)
+
+# ----- PWFSLSmoke viable version ----------------------------------------------
+
+meta <-
+  meta_ORIG %>%
+  dplyr::filter(deviceDeploymentID %in% deviceDeploymentIDs) %>%
+  dplyr::mutate(
+    siteName = locationName,
+    monitorID = sprintf("%s_%02d", AQS_AQSID, as.numeric(AQS_POC))
+  )
+
+data <- data_ORIG
+
+monitorColumnNames <- names(data)[2:ncol(data)]
+parts <- stringr::str_split_fixed(monitorColumnNames, "_", 3)
+newColumnNames <- sprintf("%s_%s", parts[,2], parts[,3])
+
+names(data) <- c('datetime', newColumnNames)
+
+preferredColumns <- c('datetime', meta$monitorID)
+
+data <-
+  data %>%
+  dplyr::select(dplyr::all_of(preferredColumns))
+
+# Remove 'tibble-ness'
+
+class(meta) <- "data.frame"
+class(data) <- "data.frame"
+rownames(meta) <- meta$monitorID
+
+# Create the 'ws_monitor' data list
+ws_monitor <- list(meta = meta, data = data)
+ws_monitor <- structure(ws_monitor, class = c("ws_monitor", "list"))
+
+PWFSLSmoke::monitor_timeseriesPlot(
+  ws_monitor, pch = 15,
+  col = adjustcolor('black', 0.1)
+)
+
+library(PWFSLSmoke)
+
+ws_monitor %>%
+  monitor_subset(stateCodes = "HI", tlim = c(20080901, 20081001)) %>%
+  monitor_leaflet()
+
+ws_monitor %>%
+  monitor_subset(stateCodes = "HI", tlim = c(20080901, 20081001)) %>%
+  monitor_subset(monitorIDs = "150012017_01") %>%
+  monitor_dailyBarplot()
+
+ws_monitor %>%
+  monitor_subset(stateCodes = "HI", tlim = c(20080901, 20081001)) %>%
+  ##monitor_subset(monitorIDs = "150012017_01") %>%
+  AirMonitorPlots::monitor_ggDailyHourlyBarplot(columns = 2)
+
+
+
+
+
 
